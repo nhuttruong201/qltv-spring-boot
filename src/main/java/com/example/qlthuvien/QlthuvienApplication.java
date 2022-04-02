4a93244a6f58ddadcb69ad4de8d16e14d2eb08df
Nhựt Trường Võ <cristruong321@gmail.com>
2022-04-02 14:20:09 +0700

    init project

---
+++ src/main/java/com/example/qlthuvien/QlthuvienApplication.java
@@ -0,0 +1,13 @@
+package com.example.qlthuvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlthuvienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlthuvienApplication.class, args);
+		System.out.println("Hello");
+	}
+}
